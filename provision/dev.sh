10228b9be5de51d66a861dcab1739971033e6aea
Caihua Yin <caihua.yin@emc.com>
2016-12-17 13:48:18 +0000

    Run store and fibnacci service by ansible

--- provision/dev.sh
+++ provision/dev.sh
@@ -4,4 +4,7 @@
 set -e
 
 echo "Installing Ansible..."
-apt-get install -y --force-yes ansible
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y --force-yes ansible
